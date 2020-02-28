79bf1a13e37046e37cf49e27c83595544da99b73
Cameron Krantz <ck283615@ohio.edu>
2020-02-28 11:11:12 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MIS4200Team9.Startup))]
+namespace MIS4200Team9
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
